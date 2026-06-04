7903de7b313e81b2ef32cbdd26110b1f2a1db3ac
Maks <m.o.kosianchuk@strudent.khai>
2026-06-04 21:15:19 +0300

    v3.1.2 | Replaced potst/saved to switch bar, fixed user page follows actions, added  _blank to lexical links

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -56,6 +56,7 @@ const Profile = () => {
                 followers: followAnotherUser?.follower?.followers
             })
         }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [followAnotherUser])
 
     useEffect(() => {
