e72fb95c935a9a4d2ca46e85f16e733432157756
Maks <m.o.kosianchuk@strudent.khai>
2026-07-22 19:27:55 +0300

    v3.9.2 | Fixed api routes, redesigned switch bar, added field component and otp input

--- src/components/Ui/TextEditorField/index.jsx
+++ src/components/Ui/TextEditorField/index.jsx
@@ -28,7 +28,6 @@ import { ReactComponent as ItalicText } from "../../../assets/svg/italic-text.sv
 import { ReactComponent as MarkList } from "../../../assets/svg/mark-list-icon.svg";
 import { ReactComponent as NumList } from "../../../assets/svg/num-list-icon.svg";
 import { ReactComponent as LinkText } from "../../../assets/svg/link-icon.svg";
-import { ReactComponent as WarningIcon } from "../../../assets/svg/warning-icon.svg";
 
 import { useState, useEffect } from "react";
 import { useRef } from "react";
