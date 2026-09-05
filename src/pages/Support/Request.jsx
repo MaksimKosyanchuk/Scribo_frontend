bedd0a0b753f99295684389e2a4e1a186a00b280
MaksimKosyanchuk <mckos2005@gmail.com>
2026-09-05 03:09:17 +0300

    Fixed lint'

--- src/pages/Support/Request.jsx
+++ src/pages/Support/Request.jsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 import { AppContext } from "../../App.jsx";
 import { getPublicSupportRequest, replyPublicSupportRequest, updateSupportRequestStatus } from "../../api/support.api";
